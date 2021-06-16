008ce9fbdf48e2a2bacc77be2481f73d80a62025
jopemachine <jopemachine@naver.com>
2021-06-16 20:03:04 +0900

    Fix bug

--- link.ts
+++ link.ts
@@ -5,7 +5,7 @@ const npmGlobal = process.env.npm_config_global;
 
 if (npmGlobal === "") {
   // Prevent linking if the script was part of a non-global npm (install) command
-  throw new Error('npmGlobal is not set!');
+  process.exit(0);
 }
 
 linkArvisGlobalModule();
