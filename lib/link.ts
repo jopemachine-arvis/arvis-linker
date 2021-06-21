9486fbb4dbb63b73b2a704da9ea5bbffbe9b03de
jopemachine <jopemachine@naver.com>
2021-06-21 13:14:29 +0900

    handle EEXIST error on Windows

--- lib/link.ts
+++ lib/link.ts
@@ -4,6 +4,13 @@ import del from "del";
 export default (src: string, dest: string) =>
   del(dest, { force: true }).then(() =>
     fs.symlink(src, dest, (err) => {
-      if (err) throw new Error(err.message);
+      if (err) {
+        // skip install symlink
+        if (err.code === "EEXIST") {
+          console.log('symlink already exists.')
+          return;
+        }
+        throw new Error(err.message);
+      }
     })
   );
