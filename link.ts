c95c4da0d8048e7a01582c43532c4feb06094dee
jopemachine <jopemachine@naver.com>
2021-06-30 22:16:15 +0900

    add travis checker

--- link.ts
+++ link.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import { linkArvisGlobalModule } from "./index";
+import isTravis from 'is-travis';
 
 const npmGlobal = process.env.npm_config_global;
 
-if (npmGlobal === "") {
+if (!npmGlobal || npmGlobal === "" || isTravis) {
   // Prevent linking if the script was part of a non-global npm (install) command
   process.exit(0);
 }
