6dfde547d7a4e63af97478b40b269b0ea0e3c4d4
jopemachine <jopemachine@naver.com>
2021-07-09 17:05:20 +0900

    fix bug

--- lib/path.ts
+++ lib/path.ts
@@ -2,11 +2,10 @@ import envPathsGenerator from 'env-paths';
 import path from 'path';
 
 const envPaths = envPathsGenerator('arvis');
-const coreModuleEnvPaths = envPathsGenerator('arvis-core');
 
 const installedDataPath = envPaths.data;
 const arvisRenewExtensionFlagFilePath = path.resolve(installedDataPath, 'arvis-extension-renew');
-const userConfigPath = path.resolve(coreModuleEnvPaths.config, 'user-configs.json');
+const userConfigPath = path.resolve(envPaths.config, 'user-configs.json');
 
 export {
   arvisRenewExtensionFlagFilePath,
